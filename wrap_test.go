e1bdaedfba9b989426951a007608a826ed55c10f
black-desk <me@black-desk.cn>
2024-05-07 14:51:33 +0800

    tests: add an example
    Signed-off-by: black-desk <me@black-desk.cn>

--- wrap_test.go
+++ wrap_test.go
@@ -6,12 +6,25 @@ import (
 	"github.com/black-desk/errwrap"
 )
 
-func ExampleWrap() {
+func ExampleWrap_with_annotation() {
 	fn := func(x int) (err error) {
 		defer errwrap.Wrap(&err, "fn(%d)", x)
 		return fmt.Errorf("error")
 	}
 	err := fn(1)
 	fmt.Println(err)
+
 	// Output: fn(1): error
+}
+
+func ExampleWrap_without_annotation() {
+
+	fn := func() (err error) {
+		defer errwrap.Wrap(&err)
+		return fmt.Errorf("error")
+	}
+	err := fn()
+	fmt.Println(err)
+
+	// Output: error
 }
